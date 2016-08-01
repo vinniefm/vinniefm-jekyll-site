b818f76cc227dbf0169e81334883433c0fe5fea6
Vince Mease <vince@lisaapp.com>
2016-08-01 02:48:05 -0400

    sticky done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,49 @@
-var sticky = new Waypoint.Sticky({
-  element: $('h3')[0],
-  offset: 20,
-  handler: function(direction){
-    $(this.element).addClass('stuck').siblings().sticky.destroy();
-  }
-});
-// sticky();
-// 
-// $('.meta-contact').waypoint.sticky(function(direction) {
-//   $(this.element).addClass('fixed-element-top');
-// }, {offset: '0'});
-// $('h3').waypoint(function(direction) {
-//   $(this.element).addClass('fixed-element-next').siblings().removeClass('fixed-element-next');
-// }, {offset: '0'});
+var stickyMetaContact = new Waypoint.Sticky({
+  element: $('.meta-contact')[0],
+  stuckClass: 'stuck-meta-contact',
+  offset: 0,
+  handler: function(direction) {
+        $("<style>")
+        .prop("type", "text/css")
+        .html("\
+        .stuck-meta-contact {\
+            display: block;\
+            float: left;\
+            position: fixed;\
+            top: 0;\
+            background-color: #f1f1f1;\
+            width:" + $(".sticky-wrapper").width() + "px;\
+        }")
+        .appendTo("head");
+    }
+})
+
+var stickyH3s = [];
+
+$('h3').each(function(i){
+  stickyH3s[i] = new Waypoint.Sticky({
+    element: this,
+    offset: $('.stuck-meta-contact').outerHeight(true)+110,
+    handler: function(direction) {
+        $("<style>")
+        .prop("type", "text/css")
+        .html("\
+        .stuck {\
+            display: block;\
+            float: left;\
+            position: fixed;\
+            top:" + $(".stuck-meta-contact").height() + "px;\
+            background-color: #f1f1f1;\
+            padding-bottom: .5rem;\
+            padding-left: 20px;\
+            margin-left: -20px;\
+            margin-right: -40px;\
+            width:" + ($(".sticky-wrapper").width()+40) + "px;\
+            -webkit-box-shadow: 0px 20px 10px -15px rgba(136,136,136,0.5);\
+            moz-box-shadow: 0px 20px 10px -15px rgba(136,136,136,0.5);\
+            box-shadow: 0px 20px 10px -15px rgba(136,136,136,0.5);\
+        }")
+        .appendTo("head");
+    }
+  })
+})
